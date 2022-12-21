a359c5529b95ac1d035a569abbadc9034321904a
Denitsa Ivanova <denitsa.ivanova@bmsys.eu>
2022-12-21 07:56:38 +0200

    Add project files.

---
+++ EnergyClaculator/Areas/Admin/Models/User/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EnergyCalculator.Areas.Admin.Models.User
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string UserName { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+    }
+}
